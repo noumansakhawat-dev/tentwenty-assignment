38ca8d7af9a1babb6ee82f07c05cf6a803c934b3
Nouman Sakhawat <48842733+noumansakhawat-dev@users.noreply.github.com>
2025-07-26 03:00:22 +0500

    feat: Add react-native-gesture-handler, reanimated, and worklets (#14)
    Added 'react-native-gesture-handler', 'react-native-reanimated', and 'react-native-worklets' to dependencies in package.json. Updated Babel config to include 'react-native-worklets/plugin' for production. Updated Podfile.lock and yarn.lock to reflect new dependencies.

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
     presets: ['module:@react-native/babel-preset'],
     env: {
       production: {
-        plugins: ['react-native-paper/babel']
+        plugins: ['react-native-paper/babel', 'react-native-worklets/plugin']
       }
     },
     plugins: [
