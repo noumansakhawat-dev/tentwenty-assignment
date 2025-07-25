a0d37e42fb6d7dc6ddb1e06a68a92c3745594d0a
Nouman Sakhawat <48842733+noumansakhawat-dev@users.noreply.github.com>
2025-07-26 01:51:11 +0500

    chore: Add module resolver and path alias support (#10)
    Configured Babel and TypeScript to support path aliases using 'babel-plugin-module-resolver' and updated tsconfig.json with matching paths. This enables cleaner and more maintainable imports from the src directory. Added the required dependency to package.json.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    env: {
+      production: {
+        plugins: []
+      }
+    },
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '~': ['./src/']
+          }
+        }
+      ]
+    ]
+  };
 };
