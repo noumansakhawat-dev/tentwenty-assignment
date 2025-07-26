06d7e63b039ede232509e7e663b976cd3072ef21
Nouman Sakhawat <dev.noumansakhawat@gmail.com>
2025-07-26 18:16:02 +0500

    feat: Add AppHeader component and integrate into navigation
    Introduced a reusable AppHeader component with support for default and center-aligned variants, including optional search functionality. Updated TabNavigator and WatchScreen to use AppHeader for consistent header UI. Extended theme with darkBlueGray color for header styling.

--- src/navigation/tabNavigator/TabNavigator.tsx
+++ src/navigation/tabNavigator/TabNavigator.tsx
@@ -2,39 +2,45 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { BottomTabBar } from './BottomTabBar';
 import { TabNavigatorParamList } from './types';
 
+import { AppHeader } from '~/components';
 import { DashboardScreen, MediaLibraryScreen, MoreScreen, WatchScreen } from '~/screens';
 
 const Tab = createBottomTabNavigator<TabNavigatorParamList>();
 
 export const TabNavigator = ({}) => {
   return (
-    <Tab.Navigator tabBar={(props) => <BottomTabBar {...props} />} initialRouteName='DashboardScreen' detachInactiveScreens>
+    <Tab.Navigator tabBar={(props) => <BottomTabBar {...props} />} initialRouteName='WatchScreen' detachInactiveScreens>
       <Tab.Screen
         name='DashboardScreen'
         component={DashboardScreen}
         options={{
-          tabBarLabel: 'Dashboard'
+          tabBarLabel: 'Dashboard',
+          header: ({ navigation }) => <AppHeader title='Dashboard' navigation={navigation} hideBackButton hideSearchButton />
         }}
       />
       <Tab.Screen
         name='WatchScreen'
         component={WatchScreen}
         options={{
-          tabBarLabel: 'Watch'
+          tabBarLabel: 'Watch',
+          headerShown: false,
+          header: ({ navigation }) => <AppHeader title='Watch' navigation={navigation} hideBackButton />
         }}
       />
       <Tab.Screen
         name='MediaLibraryScreen'
         component={MediaLibraryScreen}
         options={{
-          tabBarLabel: 'Media Library'
+          tabBarLabel: 'Media Library',
+          header: ({ navigation }) => <AppHeader title='Media Library' navigation={navigation} hideBackButton hideSearchButton />
         }}
       />
       <Tab.Screen
         name='MoreScreen'
         component={MoreScreen}
         options={{
-          tabBarLabel: 'More'
+          tabBarLabel: 'More',
+          header: ({ navigation }) => <AppHeader title='More' navigation={navigation} hideBackButton hideSearchButton />
         }}
       />
     </Tab.Navigator>
